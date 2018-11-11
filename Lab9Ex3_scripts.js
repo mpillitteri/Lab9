653b7024313c3110cbd2853f377058d6d7c5a412
Maria Pillitteri <43287010+mpillitteri@users.noreply.github.com>
2018-11-10 23:54:13 -0500

    Lab9
    Lab9

---
+++ Lab9Ex3_scripts.js
@@ -0,0 +1,15 @@
+//This will output when the page loads
+document.write("Output Zero Here");
+
+//This will output when a button is pressed
+function theRest(){
+	document.getElementById("output1").innerHTML="Output One Here";
+}
+
+function theRest(){
+	document.getElementById("output2").innerHTML="Output Two Here";
+}
+
+function theRest(){
+	document.getElementById("output3").innerHTML="Output Three Here";
+}
